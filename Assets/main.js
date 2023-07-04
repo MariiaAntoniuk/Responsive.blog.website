4d51cab988716b6db4cf587629a6796917ebecc6
MariiaAntoniuk <mariia.antonik18@gmail.com>
2023-07-04 21:10:12 +0300

    add files

---
+++ Assets/main.js
@@ -0,0 +1,18 @@
+//.......Nav Drop Down........
+
+document.querySelector('.Open').addEventListener('click',()=>{
+    document.querySelector('.nav-link').style.display='flex';
+    document.querySelector('.Open').style.display='none'
+    document.querySelector('.Close').style.display='inline-block'
+})
+
+document.querySelector('.Close').addEventListener('click',()=>{
+    document.querySelector('.nav-link').style.display='none';
+    document.querySelector('.Open').style.display='inline-block'
+    document.querySelector('.Close').style.display='none'
+})
+
+//----------inline..check-----------
+document.querySelector('.inline').addEventListener('click', ()=>{
+    document.querySelector('.inline input').classList.toggle('red')
+})
